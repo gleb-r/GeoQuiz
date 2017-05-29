cc76bbb0015b88f4d0a0f639213712cd49a552f0
gleb-r <rad.gleb@gmail.com>
2017-05-29 20:24:45 +0300

    finished exer3

--- app/src/main/java/com/example/gleb/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/gleb/geoquiz/QuizActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,7 +13,8 @@ public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private ImageButton mNextButton;
+    private ImageButton mBackButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -49,8 +51,17 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        mNextButton = (Button)findViewById(R.id.next_button);
+        mNextButton = (ImageButton)findViewById(R.id.next_button);
         mNextButton.setOnClickListener(mOnClickListener);
+
+        mBackButton = (ImageButton)findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex-1+mQuestionBank.length)%mQuestionBank.length;
+                mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+            }
+        });
 
 
 
